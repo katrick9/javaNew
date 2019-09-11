f07b9084ebcd20686ed3c9680114add2648ff4f7
katrick9 <rockstar.katrick@gmail.com>
2019-09-11 14:34:27 +0530

    first commit

---
+++ new/src/com/newest/com/NewOne.java
@@ -0,0 +1,17 @@
+package com.newest.com;
+import java.util.*;
+
+
+public class NewOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<10;i++){
+			for(int j=0;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
